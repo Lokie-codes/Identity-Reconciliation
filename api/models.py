d6f150c2e6bad1fb90693972f43f1f37c5e74461
Lokesh S <lokesh.sinduluri@gmail.com>
2025-07-26 19:54:14 +0530

    Add Contact model with fields for phone number, email, linked ID, and timestamps

---
+++ api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    id = models.AutoField(primary_key=True)
+    phoneNumber = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(max_length=254, blank=True, null=True)
+    linkedId = models.IntegerField(null=True, blank=True)
+    linkPrecedence = models.TextChoices(
+        'secondary', 
+        'primary')
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    deletedAt = models.DateTimeField(null=True, blank=True)
+    def __str__(self):
+        return f"Contact(id={self.id}, phoneNumber={self.phoneNumber}, email={self.email} linkedId={self.linkedId}, linkPrecedence={self.linkPrecedence})"
